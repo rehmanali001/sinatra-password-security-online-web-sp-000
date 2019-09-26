31f147bccafb42b31236edbbcfdf6d5ace00730c
Rehman Ali <rehmanali001@gmail.com>
2019-09-26 20:54:25 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define(version: 20180314210852) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "password_digest"
+    t.string "password"
   end
 
 end
